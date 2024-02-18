2e12926c429a94194155c5a2a70905876a60ec6c
nickanyp <85357272+nickanyp@users.noreply.github.com>
2024-02-19 00:21:06 +0700

    navbar

---
+++ lunardormitory/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+function Home() {
+  return (
+    <div>
+        <Nav></Nav>
+        <img src='https://cdn.discordapp.com/attachments/1205929906291089510/1205941611595309147/6e716d1046575431_1.png?ex=65e36e25&is=65d0f925&hm=922a61598a1cfad1e264bbefed19ec55373a37cff60ab4365edf0da255cc6a81&' alt='bg'></img>
+    </div>
+  )
+}
+
+export default Home
